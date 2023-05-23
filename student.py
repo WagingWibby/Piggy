651e3d71280570123bd2a8529b514f4f773312a9
WagingWibby <125601698+WagingWibby@users.noreply.github.com>
2023-05-23 19:18:07 +0000

    move_till_wall fix

--- student.py
+++ student.py
@@ -84,7 +84,7 @@ class Piggy(PiggyParent):
         if L > R:
           self.servo(1500)
           time.sleep(1)
-          self.turn_by_deg(-82)
+          self.turn_by_deg(-80)
           time.sleep(1)
          
           
